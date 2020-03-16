05892484e02946d8d9865f989be4b28a5e339d40
2363889777 <2363889777@qq.com>
2020-03-16 13:17:20 +0800

    文件结构

---
+++ service/src/main/java/com/sanyi/sn/Demo.java
@@ -0,0 +1,13 @@
+package com.sanyi.sn;
+
+/**
+ * @author 十年
+ * @function
+ * @date 2020/3/16 0016
+ * @place 公司
+ * @ver 1.0.0
+ * @copy 老九学堂
+ */
+public class Demo {
+    private String test = "git添加测试";
+}
